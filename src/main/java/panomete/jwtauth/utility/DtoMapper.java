61750771fd77cfc33b133fa5eea0fa97a66ca2ba
oat431 <oat431@gmail.com>
2023-06-02 02:51:01 +0700

    🔧 update: hibernate validation to starter validataion

--- src/main/java/panomete/jwtauth/utility/DtoMapper.java
+++ src/main/java/panomete/jwtauth/utility/DtoMapper.java
@@ -5,12 +5,15 @@ import org.mapstruct.factory.Mappers;
 
 @Mapper
 public interface DtoMapper {
-    DtoMapper CONVERT = Mappers.getMapper(DtoMapper.class);
+    DtoMapper INSTANCE = Mappers.getMapper(DtoMapper.class);
 
     /**
      * code of conduct for this method
      * EntityDto toEntityDto(Entity entity);
      * when it is used it will be something like this
-     * DtoMapper.CONVERT.toEntityDto(entity);
+     * DtoMapper.INSTANCE.toEntityDto(entity);
+     * example:
+     * UserDto toUserDto(User user);
+     * DtoMapper.INSTANCE.toUserDto(user);
      */
 }
